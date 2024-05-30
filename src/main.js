b24fcdc8b11b059a95bc8e195885f78606d02dd5
juric70 <andjela.juric12@gmail.com>
2024-05-30 09:41:15 +0200

    Resolved conflict

--- src/main.js
+++ src/main.js
@@ -1,22 +1,10 @@
-<<<<<<< Updated upstream
-import './assets/main.css'
-=======
+
 import "./assets/main.css";
 import { createVuetify } from "vuetify";
 import router from "../router";
 import store from "../store.js";
 import axios from "axios";
->>>>>>> Stashed changes
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import Vuetify from 'vuetify/lib'
-
-<<<<<<< Updated upstream
-const vuetify = new Vuetify({})
-
-createApp(App).use(vuetify).mount('#app')
-=======
 axios.defaults.baseURL = "http://127.0.0.1:8000/";
 axios.defaults.withCredentials = true;
 const vuetify = new createVuetify({
@@ -25,4 +13,4 @@ const vuetify = new createVuetify({
 });
 
 createApp(App).use(router).use(store).mount("#app");
->>>>>>> Stashed changes
+
